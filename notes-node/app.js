58de419982b41b6ee9f7b2b267398b53a440d5fa
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2018-10-08 20:49:09 -0500

    Read input params

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,3 +5,17 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
+var command = process.argv[2];
+console.log('Command: ', command);
+
+if (command === 'add'){
+    console.log('Adding new note');
+} else if (command === 'list'){
+    console.log('List all notes');
+} else if (command === 'remove'){
+    console.log('Remove all notes');
+} else if (command === 'read'){
+    console.log('Read all notes');
+} else {
+    console.log('No define');
+}
