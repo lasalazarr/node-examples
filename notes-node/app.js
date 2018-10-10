e784edc25ae7ac3cd29625c1d647e77399aac786
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2018-10-09 21:51:27 -0500

    Minor Fixes

--- notes-node/app.js
+++ notes-node/app.js
@@ -8,14 +8,14 @@ const notes = require('./notes.js');
 var command = process.argv[2];
 console.log('Command: ', command);
 
-if (command === 'add'){
-    console.log('Adding new note');
-} else if (command === 'list'){
-    console.log('List all notes');
-} else if (command === 'remove'){
-    console.log('Remove all notes');
-} else if (command === 'read'){
-    console.log('Read all notes');
-} else {
-    console.log('No define');
-}
+// if (command === 'add'){
+//     console.log('Adding new note');
+// } else if (command === 'list'){
+//     console.log('List all notes');
+// } else if (command === 'remove'){
+//     console.log('Remove all notes');
+// } else if (command === 'read'){
+//     console.log('Read all notes');
+// } else {
+//     console.log('No define');
+// }
