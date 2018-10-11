0daf2966bafc220e024b60976f40f136312394b7
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2018-10-11 15:52:32 -0500

    Add Yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,20 +2,25 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log('Command: ', command);
+console.log('Process: ', process.argv);
+console.log('Yargs', argv);
 
-// if (command === 'add'){
-//     console.log('Adding new note');
-// } else if (command === 'list'){
-//     console.log('List all notes');
-// } else if (command === 'remove'){
-//     console.log('Remove all notes');
-// } else if (command === 'read'){
-//     console.log('Read all notes');
-// } else {
-//     console.log('No define');
-// }
+if (command === 'add'){
+    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list'){
+    console.log('List all notes');
+} else if (command === 'remove'){
+    console.log('Remove all notes');
+} else if (command === 'read'){
+    console.log('Read all notes');
+} else {
+    console.log('No define');
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -2,11 +2,19 @@ console.log('Starting notes.js');
 
 //module.exports.age = 25;
 
-module.exports.addNote = () =>{
-    console.log('addNote');
-    return 'New note';
+// module.exports.addNote = () =>{
+//     console.log('addNote');
+//     return 'New note';
+// };
+
+// module.exports.add = (a,b) =>{
+//     return a+b;
+// };
+
+var addNote =(title, body) => {
+    console.log('Adding note', title, body);
 };
 
-module.exports.add = (a,b) =>{
-    return a+b;
+module.exports = {
+    addNote
 };
